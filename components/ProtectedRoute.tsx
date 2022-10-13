f33413cb9aa3cb0491e256b1bf4640d6bc4f4d7b
sidney-m-grant <109319904+sidney-m-grant@users.noreply.github.com>
2022-10-13 16:52:41 -0700

    bug fix protected route

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -1,22 +1,20 @@
-import React, { useEffect } from 'react'
-import { useAuth } from '../context/AuthContext'
-import { useRouter } from 'next/router'
+import React, { useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useRouter } from "next/router";
 
-const ProtectedRoute = ({children}: {children: React.ReactNode}) => {
-    const { user } = useAuth()
-    const router = useRouter()
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const { user } = useAuth();
+  const router = useRouter();
 
-    // basic use effect sends us back to login if no one is signed in
+  // basic use effect sends us back to login if no one is signed in
 
-    useEffect(() => {
-        if(!user) {
-            router.push('/signIn')
-        }
-    }, [router, user])
+  useEffect(() => {
+    if (!user) {
+      router.push("/SignIn");
+    }
+  }, [router, user]);
 
-    return (
-        <>{user ? children : null}</>
-    )
-}
+  return <>{user ? children : null}</>;
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
